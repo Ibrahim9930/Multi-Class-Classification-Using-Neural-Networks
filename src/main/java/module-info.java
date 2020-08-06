20007a946acd283958cf18e5e4d44d6c30fd53e7
Ibrahim Masri <msri.ibraheem@gmail.com>
2020-08-06 18:46:52 +0300

    fixed moduling problem

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.uousef.project.ai {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires com.jfoenix;
+
+    opens org.uousef.project.ai;
+}
