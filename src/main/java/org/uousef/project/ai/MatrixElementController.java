6e5eee3f5c9087166a2ce2abfa600356be70c5bb
YousefAslan <YousefAslan9@gmail.com>
2020-08-08 03:13:00 +0300

    change the confusion matrix colors

--- src/main/java/org/uousef/project/ai/MatrixElementController.java
+++ src/main/java/org/uousef/project/ai/MatrixElementController.java
@@ -2,6 +2,7 @@ package org.uousef.project.ai;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class MatrixElementController {
@@ -20,16 +21,18 @@ public class MatrixElementController {
         fontSize = (fontSize >= 1) ? 18 : 18 * fontSize;
 
 
-        this.intensity = (value * (60 / (double) max)) + 10;
-        intensity = 100 - intensity;
+        this.intensity = (value * (80 / (double) max)) + 5;
+//        intensity = 100 - intensity;
 
         textValue.setText(String.valueOf(this.value));
         elementBackground.setPrefHeight(size);
         elementBackground.setPrefWidth(size);
-        elementBackground.setStyle("-fx-background-color: hsb(221, 67%," + intensity + "%);"
+        textValue.setFill(Color.web("#14213D"));
+        elementBackground.setStyle("-fx-background-color: hsb(37," + intensity + "%,99%);"
                 + "-fx-font-size:" + fontSize + ";");
     }
 
+    //37, 84, 99
     void setupString(String string, int number, double size) {
         double fontSize = size / 70.0;
         fontSize = (fontSize >= 1) ? 18 : 18 * fontSize;
@@ -38,8 +41,11 @@ public class MatrixElementController {
         textValue.setText(string);
         elementBackground.setPrefHeight(size);
         elementBackground.setPrefWidth(size);
-        elementBackground.setStyle("-fx-background-color: hsb(221, 67%, 85%);"
-                + "-fx-font-size:" + fontSize + ";");
+//        221, 67, 39
+//        elementBackground.setStyle("-fx-background-color: hsb(221, 67%, 69%);"
+        elementBackground.setStyle("-fx-background-color: #233A6C;"
+                + "-fx-font-size:" + fontSize + ";"
+                + "-fx-border-color: black");
     }
 
 }
